c5d60ab4010e8dfcff6e1727f57aff63e06b2844
Alexian Hentiu <alexianhentiu1208@gmail.com>
2020-12-02 14:44:55 +0200

    update

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,4 +4,5 @@ echo "Enter your password when asked and always go with the default selection!"
 git clone https://aur.archlinux.org/yay ~/Downloads/yay
 cd ~/Downloads/yay
 makepkg -si
-yay -S $(grep . ../package_list.txt | grep -v "^#")
+cd -
+yay -S $(grep . ./package_list.txt | grep -v "^#")
