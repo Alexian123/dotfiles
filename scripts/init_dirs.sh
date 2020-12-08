3d5481665cf84da065128e92d81a87e56725419f
Alexian Hentiu <alexianhentiu1208@gmail.com>
2020-12-08 22:22:06 +0200

    restructuring

--- scripts/init_dirs.sh
+++ scripts/init_dirs.sh
@@ -1,50 +1,20 @@
 #!/bin/bash
 
-# /usr/share
-sudo mkdir /usr/share/avatars
-sudo mkdir /usr/share/wallpapers
-
-# .config
-mkdir ~/.config/nvim -p
-mkdir ~/.config/i3
-mkdir ~/.config/i3blocks
-mkdir ~/.config/qtile
-mkdir ~/.config/openbox
-mkdir ~/.config/ranger
-mkdir ~/.config/gxkb
-mkdir ~/.config/tint2
-mkdir ~/.config/codeblocks
-mkdir ~/.config/gtk-2.0
-mkdir ~/.config/gtk-3.0
-mkdir ~/.config/qt5ct/colors -p
-mkdir ~/.config/qt5ct/qss
-mkdir ~/.config/alacritty
-mkdir ~/.config/htop
-
-# .cache
+# neovim undodir
 mkdir ~/.cache/nvim/undodir -p
 
-# .local
-mkdir ~/.local/bin -p
-mkdir ~/.local/share/applications -p
-mkdir ~/.local/share/fonts
-
-# .screenlayout
-mkdir ~/.screenlayout
-
-# Projects
+# Projects dir
 mkdir ~/Projects/CodeBlocks -p
 mkdir ~/Projects/Eclipse
 mkdir ~/Projects/VSCode
-
-# Source
-mkdir ~/Source/personal/dotfiles -p
+mkdir ~/Projects/VSCode/python
+mkdir ~/Projects/VSCode/haskell
 
 # VMs
 mkdir ~/VirtManager\ VMs
 mkdir ~/VirtualBox\ VMs
 
-# Pictures
+# screenshots dir
 mkdir ~/Pictures/screenshots -p
 
 # Documents
