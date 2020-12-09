a21ceebbb421cf851137c73a462fb41cf478c334
Alexian Hentiu <alexianhentiu1208@gmail.com>
2020-12-09 20:46:30 +0200

    moving current dotfiles to new repo

--- scripts/install_dependencies.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Enter your password when asked and always go with the default selection!"
-git clone https://aur.archlinux.org/yay ~/Downloads/yay
-cd ~/Downloads/yay
-makepkg -si
-cd -
-yay -S $(grep . ./package_list.txt | grep -v "^#")
--- scripts/sync_dotfiles.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# track all new files, commit changes and push to remote repo on github 
-# requires approved ssh key
-
-cd $DOTFILES
-git add --all
-git commit -a -m "update"
-git push
